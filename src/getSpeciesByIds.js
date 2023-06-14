fffbdc489edbb62a4f051d674ace10a78e86640d
AnaCarolineOliv <ana.caroline.ifg.edu@gmail.com>
2023-06-14 18:01:48 -0300

    1.Implemente a função getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,18 @@ const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => {
   // seu código aqui
+  // console.log(ids);
+  if (ids.length === 0) {
+    return [];
+  }
+  const species = data.species.filter((specie) => ids.some((id) => id === specie.id));
 
+  return species;
+  // return species.map((population) => {
+  //   console.log(population.id);
+  //   return population.id === ids;
+  // });
 };
-
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+// console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
