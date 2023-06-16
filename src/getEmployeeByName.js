bcc211279f857452ef8e64444038cf8b9f5f24cc
AnaCarolineOliv <ana.caroline.ifg.edu@gmail.com>
2023-06-16 11:02:46 -0300

    Req3. Implemente a função getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees
+    .find((employe) => employe.firstName === employeeName || employe.lastName === employeeName);
 };
 
+// const getEmployeeByName = (employeeName) => employeeName === undefined ? [{}] : data.employees
+//     .find((employe) => employe.firstName === employeeName || employe.lastName === employeeName);
+
+console.log(getEmployeeByName('Nelson'));
 module.exports = getEmployeeByName;
