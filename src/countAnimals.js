e60212edc3b80e671f12ea4e61fdd4d7f4f2cab5
AnaCarolineOliv <ana.caroline.ifg.edu@gmail.com>
2023-06-21 21:50:03 -0300

    Req.5-Implemente a função countAnimals.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+// const conter = () => {
+
+// };
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  const animalUndefined = typeof animal === 'undefined';
+  const { species } = data;
+  if (!animalUndefined) {
+    const specieOfAnimal = data.species.find((specie) => specie.name === animal.species);
+    if (typeof animal.sex !== 'undefined') {
+      return specieOfAnimal.residents.filter((resident) => resident.sex === animal.sex).length;
+    }
+    return specieOfAnimal.residents.length;
+  }
+  return species.reduce((curr, specie) => {
+    const { name, residents } = specie;
+    const newCurr = {
+      [`${name}`]: residents.length,
+    };
+    return { ...curr, ...newCurr };
+  }, {});
 };
 
+// console.log(countAnimals({ species: 'lions', sex: 'female' }));
+// countAnimals({ species: 'lions' });
+// console.log(countAnimals());
+// countAnimals();
 module.exports = countAnimals;
