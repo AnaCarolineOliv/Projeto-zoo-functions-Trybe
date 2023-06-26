c8dcf922a2f2e8c2342087078ce86073037603f5
AnaCarolineOliv <ana.caroline.ifg.edu@gmail.com>
2023-06-26 16:06:47 -0300

    Req.15 - Obtenha 100% de cobertura de testes na função handlerElephants

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,16 +1,20 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  test('Verifica se a função HandlerElephants existe', () => {
+  test('Verifica se a funçãoexiste', () => {
     expect(typeof handlerElephants).toBe('function');
   });
-  test('A função handlerElephants retorna corretamente quando não é definido o argumento, ou não é string', () => {
+  test('A função retorna corretamente quando não é definido o argumento, ou não é string', () => {
     expect(handlerElephants()).toBe(undefined);
     expect(handlerElephants(7)).toEqual('Parâmetro inválido, é necessário uma string');
   });
-  test('A função handlerElephants retorna corretamente', () => {
+  test('A função retorna corretamente quando é um argumento ou propriedade de dos elephants', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
-    expect(handlerElephants('averageAge')).toBe(10.5);
+    expect(handlerElephants('averageAge')).toEqual(10.5);
     expect(handlerElephants('count')).toEqual(4);
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+  test('A função retorna null quando a string não é uma propriedade, nem um argumento', () => {
+    expect(handlerElephants('peso')).toBe(null);
   });
 });
